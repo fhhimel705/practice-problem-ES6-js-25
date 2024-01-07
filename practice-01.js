894dd42775e0e28895c0c9cdb3d71348e3425a51
FH Himel <fhhimel17@gmail.com>
2024-01-07 20:13:03 +0600

    added-final

---
+++ practice-01.js
@@ -0,0 +1,23 @@
+/*
+1) How will you access Sophia’s secondary school location?
+*/
+let data = {
+  Sophia: {
+    id: 33,
+    study: [
+      {
+        primary: [
+          { school_name: "ABC primary school" },
+          { location: "Peters burg" },
+        ],
+      },
+      {
+        secondary: [
+          { school_name: "ABC secondary school" },
+
+          { location: "St Lorence" },
+        ],
+      },
+    ],
+  },
+};
